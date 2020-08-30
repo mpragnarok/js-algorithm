ec5edee9fe0228eb985974aa788f8f59c05d2291
minaH <mina.h@amaryllo.us>
2020-08-30 16:07:46 +0800

    Init project and Add big o notaion folder

---
+++ big-o-notation/Add2.js
@@ -0,0 +1,9 @@
+const { PerformanceObserver, performance } = require("perf_hooks");
+
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
+let t1 = performance.now();
+addUpTo(100000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
