f3fe9eb62ea18ffab4b6c19e1e826a3cf6aee552
mina.h <mina.h@amaryllo.us>
2020-09-15 11:51:29 +0800

    <recursion><add basic recursion functions>

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -7,3 +7,13 @@ function countDown(num) {
     num--;
     countDown(num);
 }
+
+function sumRange(num) {
+    if (num === 1) return 1;
+    return num + sumRange(num - 1);
+}
+sumRange(3);
+// sumRange(3);
+//         return 3 + sumRange(2)
+//                             return 2 + sumRange(1)
+//                                                return 1
