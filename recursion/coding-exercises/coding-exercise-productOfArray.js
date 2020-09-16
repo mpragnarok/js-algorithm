6c4da5d71fa41006671c78510e88e66d41788e3b
mina.h <mina.h@amaryllo.us>
2020-09-16 17:55:31 +0800

    <update><recursion><productOfArray>

--- recursion/coding-exercises/coding-exercise-productOfArray.js
+++ recursion/coding-exercises/coding-exercise-productOfArray.js
@@ -2,6 +2,7 @@
 Write a function called `productOfArray` which takes in an array of numbers and returns the product of them all.
  */
 
+// helper method recursion
 function productOfArray(arr) {
     let product = 1;
     function helper(helperInput) {
@@ -12,6 +13,8 @@ function productOfArray(arr) {
     helper(arr);
     return product;
 }
+
+//TODO: pure recursion
 
 console.log(productOfArray([1, 2, 3])); // 6
 console.log(productOfArray([1, 2, 3, 10])); // 60
