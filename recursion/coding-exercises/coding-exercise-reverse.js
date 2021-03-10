d553ad851e455daf57684ba53275d02d93229e95
mina.h <mina.h@amaryllo.us>
2021-03-10 11:58:34 +0800

    <add><recursion, hackerrank count-valleys>

---
+++ recursion/coding-exercises/coding-exercise-reverse.js
@@ -0,0 +1,19 @@
+/*reverse
+Write a recursive function called **reverse**  which accepts a string and returns a new string in reverse.
+ */
+
+// helper method
+function reverse(str) {
+    let reverseStr = "";
+    function helper(helperInput) {
+        if (helperInput.length === 0) return;
+        // TODO: keep going here
+
+        // helper(helperInput);
+    }
+}
+
+// pure recursion
+
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
