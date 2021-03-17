d723b5a5100dcdd7ff6fa71c266af6bc8274eb6b
mpragnarok <mp922352612@gmail.com>
2021-03-17 16:48:16 +0800

    <add><comments><linear search>

--- searching/linearSearch.ts
+++ searching/linearSearch.ts
@@ -1,3 +1,4 @@
+// best: O(1), Average: O(n), Worst: O(n)
 function linearSearch(arr: any[], val: any): number {
   let index: number = -1;
   for (let i = 0; i < arr.length; i++) {
@@ -6,6 +7,13 @@ function linearSearch(arr: any[], val: any): number {
     }
   }
   return index;
+}
+
+function stephenLinearSearch(arr: any[], val: any): number {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) return i;
+  }
+  return -1;
 }
 
 linearSearch([10, 15, 20, 25, 30], 15);
